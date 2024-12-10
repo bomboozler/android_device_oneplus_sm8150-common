3c436a26270f45077282b5097f1e3e822b0d70d0
ppanzenboeck <panz.development@gmail.com>
2024-12-10 14:08:15 +0100

    cherrypicked panz viper4android modification

--- common.mk
+++ common.mk
@@ -16,7 +16,13 @@ TARGET_NOT_USES_BLUR=true
 PRODUCT_PACKAGES += \
     RemovePackages
 
+<<<<<<< HEAD
 >>>>>>> a93a7c7 (RemovePackages: Google apps debloat)
+=======
+# ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+>>>>>>> 16c5ebf (Introduce Viper4Android)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
@@ -75,7 +81,7 @@ PRODUCT_PACKAGES += \
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8150/audio
 
 PRODUCT_COPY_FILES += \
-    $(AUDIO_HAL_DIR)/configs/msmnile/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(AUDIO_HAL_DIR)/configs/msmnile/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(AUDIO_HAL_DIR)/configs/msmnile/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_ODM)/etc/audio_io_policy.conf \
